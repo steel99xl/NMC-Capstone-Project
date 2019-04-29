e60d2b15764975e24086651b43071b4b72b74744
steel99xl <steel99xl@gmail.com>
2019-04-29 10:21:44 -0400

    Update Capstone.py

--- Python stuff/Capstone.py
+++ Python stuff/Capstone.py
@@ -10,7 +10,7 @@ def accept_incoming_connections():
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        client.send(bytes("Welcome to the server. Now type your name and press enter!", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
